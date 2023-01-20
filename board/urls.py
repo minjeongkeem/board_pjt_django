f6bd6b0ebaea3c497e06d081862347c60e71c625
Jae Won Song <jwsonghub@gmail.com>
2023-01-20 17:31:09 +0900

    17:30

--- board/urls.py
+++ board/urls.py
@@ -4,7 +4,7 @@ from . import views
 app_name = 'board'
 
 urlpatterns = [
-   path('board/', views.board_create, name='views.board_create'),
+   path('create/', views.board_create, name='board_create'),
    path('', views.board_index, name='board_index'),   
    path('<int:board_edit_pk>/edit/', views.board_edit, name = 'board_edit'),
    path('<int:board_delete_pk>/delete/', views.board_delete, name = 'board_delete'),    
