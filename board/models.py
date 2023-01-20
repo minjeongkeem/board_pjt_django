4cc69406baefd2c2a31ea8844d17eae1f915d7f4
Jin <rhkdghks21@naver.com>
2023-01-20 17:31:00 +0900

    ~delete

--- board/models.py
+++ board/models.py
@@ -6,7 +6,8 @@ class Article(models.Model):
     name = models.CharField(max_length=10)
     Language = models.CharField(max_length=10,)
     content = models.TextField()
-    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='articles')
+    like_users = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name='like_articles')
 
 
 class Comment(models.Model):
--- board/urls.py
+++ board/urls.py
@@ -5,7 +5,8 @@ app_name = 'board'
 
 urlpatterns = [
    path('board/', views.board_create, name='views.board_create'),
-   path('', views.board_index, name='board_index'),   
-   path('<int:board_edit_pk>/edit/', views.board_edit, name = 'board_edit'),
+   path('', views.board_index, name='board_index'),
+   path('<int:article_pk>/', views.board_detail, name = 'board_detail'),
+   path('<int:article_pk>/delete/', views.board_delete, name = 'board_delete'),
    path('<int:board_delete_pk>/delete/', views.board_delete, name = 'board_delete'),    
 ]
